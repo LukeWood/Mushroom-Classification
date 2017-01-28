2d171184f77c86f4fd9110abf2a45f15dd0f53aa
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2017-01-28 13:02:53 -0600

    histogram_data working

--- analysis/histogram_analysis.py
+++ analysis/histogram_analysis.py
@@ -1,8 +1,19 @@
 import sys
 import os
-sys.path.append(os.path.abspath('..'))
+import numpy as np
 
+sys.path.append(os.path.abspath('..'))
 from preprocessing import shroom_dealer
 
 attr_map = shroom_dealer.get_attribute_dictionary()
-def = shroom_deal.get_data_frame()
+df = shroom_dealer.get_data_frame()
+
+hist_data = dict([(atr,None) for atr in attr_map])
+
+for x in attr_map:
+    counts = dict([(attr_map[x][y],0) for y in attr_map[x]])
+    for row in df[x]:
+        counts[attr_map[x][row]]+=1
+    hist_data[x] = dict([(c,counts[c]/df.shape[0]) for c in counts])
+
+print(hist_data)
--- preprocessing/shroom_dealer.py
+++ preprocessing/shroom_dealer.py
@@ -9,6 +9,4 @@ def get_attribute_dictionary():
 def get_data_frame():
     attribute_names = [x.split(":")[0] for x in open("../raw_data/descriptors.txt")]
 
-    df = pandas.read_csv("../raw_data/agaricus-lepiota.data.txt",names=attribute_names)
-
-    return df
+    return pandas.read_csv("../raw_data/agaricus-lepiota.data.txt",names=attribute_names)
