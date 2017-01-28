0e4075358586b5bd0bde2c05d738a53a2b59bdcb
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2017-01-28 15:19:48 -0600

    barcharts

--- visualizations/histogram.py
+++ visualizations/histogram.py
@@ -5,8 +5,6 @@ sys.path.append(os.path.abspath('..'))
 
 from analysis import histogram_analysis
 
-import matplotlib.pyplot as plt
-
 data,poison_data = histogram_analysis.get_hist_data()
 
 tf_tpf = {}
@@ -14,11 +12,22 @@ tf_tpf = {}
 for val in data:
     tf_tpf[val] = dict([(x,poison_data[val][x]/data[val][x]) for x in data[val] if data[val][x] != 0])
 
-all_corrs =[]
+import numpy as np
+import matplotlib.pyplot as plt
 
 for key in tf_tpf:
+    all_corrs = []
     for x in tf_tpf[key]:
         all_corrs.append((x,tf_tpf[key][x]))
 
-all_corrs = sorted(all_corrs, key=lambda x: x[1])
-print(all_corrs)
+    all_corrs = sorted(all_corrs, key=lambda x: x[1])
+    bar_width = 0.5
+
+    fig = plt.figure()
+
+    plt.title('Attributes Sorted by Total Frequency/Total Poison Frequency')
+    rects1 = plt.bar(np.arange(len(all_corrs))+bar_width ,[x[1] for x in all_corrs],bar_width)
+    plt.xlabel("Percentage of Occurences that were Poisonous")
+    plt.xticks(np.arange(len(all_corrs))+bar_width*3/2, [x[0] for x in all_corrs])
+
+    plt.savefig('barcharts/%s.png' % (key), dpi=fig.dpi)
