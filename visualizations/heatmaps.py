9c0d5536cbf144addb9172ac603f8279ac806ee0
Justin Ledford <justinledford@gmail.com>
2017-01-29 14:38:02 -0600

    generalize heatmaps

--- visualizations/heatmaps.py
+++ visualizations/heatmaps.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import itertools
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,23 +9,33 @@ import pandas as pd
 sys.path.append(os.path.abspath('..'))
 from preprocessing import shroom_dealer
 
-def heatmap_p_vs_e(attribute):
+def heatmap(attr1, attr2):
     df = shroom_dealer.get_data_frame()
-    labels = shroom_dealer.get_attribute_dictionary()[attribute]
-    p_data = df[attribute][df['poisonous'] == 'p'].value_counts()
-    e_data = df[attribute][df['poisonous'] == 'e'].value_counts()
-    
-    data = pd.concat([p_data, e_data], axis=1)
-    data.columns = ['poisonous', 'edible']
 
-    ticks = [labels[a] for a in data.index]
+    labels1 = shroom_dealer.get_attribute_dictionary()[attr1]
+    labels2 = shroom_dealer.get_attribute_dictionary()[attr2]
 
-    sns.heatmap(data, annot=True, yticklabels=ticks, fmt='g')
+    data = []
 
+    for a in df[attr1].cat.categories:
+        column = df[attr2][df[attr1] == a].value_counts()
+        data.append(column)
+
+    d = pd.concat(data, axis=1)
+    d.columns = [labels1[a] for a in df[attr1].cat.categories]
+
+    ticks = [labels2[a] for a in d.index]
+
+    sns.heatmap(d, annot=True, yticklabels=ticks, fmt='g')
+
+    plt.title("{} and {}".format(attr1, attr2))
     plt.yticks(rotation=0)
     plt.tight_layout()
     plt.show()
 
 
-if __name__ == '__main__':
-    heatmap_p_vs_e('odor')
+def heatmap_all():
+    cat_names = shroom_dealer.get_attribute_dictionary().keys()
+    combos = itertools.combinations(cat_names, 2)
+    for attr1, attr2 in combos:
+        heatmap(attr1, attr2)
