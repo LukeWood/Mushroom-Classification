946ebd164ba8513cbb3bd769ab90b9bf4c96697d
Lucas Wood <lucaswood@Lucass-MacBook-Pro.local>
2017-01-29 20:44:57 -0600

    graph

--- analysis/edge_analysis.py
+++ analysis/edge_analysis.py
@@ -7,7 +7,6 @@ from preprocessing import shroom_dealer
 
 attr_names= shroom_dealer.get_attribute_dictionary()
 
-
 nodes = {}
 for x in attr_names:
     for y in attr_names[x]:
@@ -20,7 +19,10 @@ for index, row in df.iterrows():
         for attr2 in attr_names:
             if(attr == attr2):
                 continue
-            nodes[attr_names[attr][row[attr]]][attr_names[attr2][row[attr2]]] = True
+            if(attr_names[attr2][row[attr2]] not in nodes[attr_names[attr][row[attr]]]):
+                nodes[attr_names[attr][row[attr]]][attr_names[attr2][row[attr2]]] = 0
+            nodes[attr_names[attr][row[attr]]][attr_names[attr2][row[attr2]]]+=1
 
-with open("../visualizations/edge-chart/edges.json","w") as f:
-    f.write(json.dumps([{"name":x,"imports":[y for y in nodes[x]]} for x in nodes]))
+for MIN in range(0,100):
+    with open("../visualizations/edge-chart/edge_data/edges%d.json" % (MIN*25),"w") as f:
+        f.write(json.dumps([{"name":x,"imports":[y for y in nodes[x] if nodes[x][y] >= MIN*25]} for x in nodes]))
